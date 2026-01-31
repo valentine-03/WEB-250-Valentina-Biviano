f242b95c24fb73802f61e5edbca5d667771afed8
Valentina Biviano <valenbiviano@gmail.com>
2026-01-30 23:33:23 -0600

    Add files via upload

---
+++ Assignment2/hello.php
@@ -0,0 +1,11 @@
+<?php
+
+// Displays "Hello world!"
+//
+// References:
+//  https://repl.it/languages/php7
+//  https://www.php.net/manual/en/tutorial.firstpage.php
+
+echo "Hello code world!";
+
+?>
---
+++ Assignment2/template.php
@@ -0,0 +1,18 @@
+<?php 
+
+$greeting = "Hello";
+$name = "world";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <p><?=$greeting?> template <?=$name?>!</p>
+  </body>
+</html>
